3efc98c9e7d24f620b4c955983ab39fa1df0f250
Wyatt Goodwin <wog6@cornell.edu>
2019-09-06 22:11:30 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,13 @@
 def turn_count(board)
 counter = 0
   board.each do |position|
-    if position !== "" || position !== " " || position !== nil
+    if position == "X" || position == "O"
       counter += 1
     end
   end
-return counter
+  counter
 end
 
-def current_player(turns)
-  player = turns % 2 == 0 ? "X" : "O"
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
